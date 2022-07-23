ea7c957b7a689e01d3a780a1a79923609624acca
greenhandatsjtu <sunhengke@sjtu.edu.cn>
2022-07-23 16:51:13 +0800

    feat: integrate with poem-casbin middleware

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ mod auth;
 
 use poem::web::Data;
 use poem::{get, handler, listener::TcpListener, web::Path, EndpointExt, Route, Server};
-use poem_casbin_auth::CasbinVals;
+use poem_casbin_auth::casbin::function_map::key_match2;
+use poem_casbin_auth::casbin::{CoreApi, DefaultModel, FileAdapter};
+use poem_casbin_auth::{CasbinService, CasbinVals};
 use std::env;
 
 #[handler]
@@ -30,11 +32,26 @@ async fn main() -> Result<(), std::io::Error> {
     if env::var_os("RUST_LOG").is_none() {
         env::set_var("RUST_LOG", "poem=debug");
     }
+    let m = DefaultModel::from_file("rbac_with_pattern_model.conf")
+        .await
+        .unwrap();
+    let a = FileAdapter::new("rbac_with_pattern_policy.csv");
+
+    let casbin_middleware = CasbinService::new(m, a).await.unwrap();
+
+    casbin_middleware
+        .write()
+        .await
+        .get_role_manager()
+        .write()
+        .matching_fn(Some(key_match2), None);
+
     let app = Route::new()
         .at("/pen/1", get(pen1))
         .at("/pen/2", get(pen2))
         .at("/book/:id", get(book))
         .at("/user", get(user))
+        .with(casbin_middleware)
         .with(auth::BasicAuth);
     Server::new(TcpListener::bind("127.0.0.1:3000"))
         .name("poem-casbin-demo")
